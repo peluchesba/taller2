86b8210e857c85c213c5b92d1745918326ace671
sebastian <sevamuoz@gmail.com>
2020-01-03 23:29:50 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HogarComponent } from './hogar/hogar.component';
+import { IndustrialComponent } from './industrial/industrial.component';
+import { NovedadesComponent } from './novedades/novedades.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'novedades', component: NovedadesComponent },
+  { path: 'hogar',component: HogarComponent },
+  { path: 'industrial',component: IndustrialComponent},
+  { path: '',component: InicioComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactoComponent,
+    FooterComponent,
+    HeaderComponent,
+    HogarComponent,
+    IndustrialComponent,
+    NovedadesComponent,
+    NosotrosComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/hogar/hogar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hogar',
+  templateUrl: './hogar.component.html',
+  styleUrls: ['./hogar.component.css']
+})
+export class HogarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
